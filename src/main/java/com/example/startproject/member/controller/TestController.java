bd01b5703b297a09adc9890a4a803a0294ab5949
hyuntroll <83080414+hyuntroll@users.noreply.github.com>
2025-05-24 01:52:01 +0900

    test controller

--- src/main/java/com/example/startproject/member/controller/TestController.java
+++ src/main/java/com/example/startproject/member/controller/TestController.java
@@ -2,13 +2,34 @@ package com.example.startproject.member.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class TestController {
-    @GetMapping("/newThing")
+    @GetMapping("/new-thing")
     public String test() {
-        return "redirect:/index.html";
+        return "index.html";
+    }
+
+    @GetMapping("new/login")
+    public String loginPage() {
+        return "redirect:/login-form.html";
+    }
+    @PostMapping("new/login")
+    public String loginProcess(
+            @RequestParam String id,
+            @RequestParam String password,
+            Model model
+    ) {
+        if ( id.equals(password) ) {
+            model.addAttribute("loginId", id);
+        }
+
+        return "login-result";
     }
 
 }
